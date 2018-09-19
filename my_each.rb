4ad10c17a95186b9b62002432bb84507490c2dde
Veniel Dorestave <veniel@noukod.com>
2018-09-19 19:51:03 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+list = ["ab", "cd", "ef"]
+
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
 end
+hello(list) {|i| "Hello #{i} what's up?"}
