66899d40cb3c2138108c65056c3ad75725b28c8a
Samuel Cornet <samuel@noukod.com>
2018-09-20 15:29:39 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,18 @@
 
 def my_each(collection)
-  my_each(collection) do |i|
-     puts i
-  end
+  # if block_given?
+    i = 0
+    while i < collection.length
+      yield(collection[i])
+      i = i + 1
+    end
+  # else
+  #   return "Sorry, your code has no give block!"
+  # end
+  collection
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
