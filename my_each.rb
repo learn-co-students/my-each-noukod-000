9da6dc6cfcf9ebbf0dd9a783f260a5e4ac6d20d9
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-08 19:42:16 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+array_yield = ['arel', 'jon', 'logan', 'spencer']
+
+def my_each(yield_arg) # put argument(s) here
+  counter = 0
+  while counter < yield_arg.length
+    yield(yield_arg[counter])
+    counter += 1
+  end
+  yield_arg
 end
+
+my_each(array_yield) {|container| "So I think #{container} is a piece of shit"}
