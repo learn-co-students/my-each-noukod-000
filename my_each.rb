2c373c12c3dec1e2c36a700d663f0421df59add4
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-15 12:22:35 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(array) here
   # code here
+  counter = 0 
+  while counter < array.size 
+  block_given?
+  yield(array[counter])
+  counter += 1 
+end
+array
 end
