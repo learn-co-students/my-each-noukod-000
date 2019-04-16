7768ddec3b00f25cad2090e9d45083e45a7ea019
James Declerus <james.declerus@esih.edu>
2019-04-16 13:03:46 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+ # put argument(s) here
+
+
+def my_each(collection)
+  i = 0
+  while i < collection.length
+# The "while" method will always return a value of => nil"
+    yield(collection[i])
+    puts i = i + 1
+  end
 end
